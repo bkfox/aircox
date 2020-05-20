20d8ddd602f2b7f0eb028cb9300c8e5bda0612d1
bkfox <thomas bkfox net>
2020-05-20 15:21:28 +0200

    missing import

--- aircox/models/__init__.py
+++ aircox/models/__init__.py
@@ -4,7 +4,7 @@ from .program import Program, Stream, Schedule
 from .episode import Episode, Diffusion
 from .log import Log
 from .sound import Sound, Track
-from .station import Station
+from .station import Station, Port
 
 from . import signals
 
