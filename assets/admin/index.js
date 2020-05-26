18e70ebbaf1b5f4d6800a31f6a0fc2dd2bc1689d
bkfox <thomas bkfox net>
2020-05-26 21:27:58 +0200

    add filtering on dropdown menu

--- assets/admin/index.js
+++ assets/admin/index.js
@@ -8,6 +8,24 @@ Vue.component('a-statistics', Statistics)
 
 import 'public';
 
+window.aircox_admin = {
+    /**
+     * Filter items in the parent navbar-dropdown for provided key event on text input
+     */
+    filter_menu: function(event) {
+        var filter = new RegExp(event.target.value, 'gi');
+        var container = event.target.closest('.navbar-dropdown');
+
+        if(event.target.value)
+            for(var item of container.querySelectorAll('a.navbar-item'))
+                item.style.display = item.innerHTML.search(filter) == -1 ? 'none' : null;
+        else
+            for(var item of container.querySelectorAll('a.navbar-item'))
+                item.style.display = null;
+    },
+
+}
+
 
 
 
